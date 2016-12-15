58f4789f290aff08479061376ae937420214f7b7
Yulia Markina <jmarkina@gmail.com>
2016-12-15 14:45:21 -0800

    submenu nav

--- assets/js/lib/main.js
+++ assets/js/lib/main.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-
+	$('#programsNavItem').mouseenter(function(e) {
+		$('#programsNav').css('display', 'block');
+		$('.nav-programs a').css('background-color', '#F8F8F7')
+	});
+	$('#programsNav').mouseleave(function(e) {
+		$('#programsNav').css('display', 'none');
+		$('.nav-programs a').css('background-color', 'white')
+	});
 });
