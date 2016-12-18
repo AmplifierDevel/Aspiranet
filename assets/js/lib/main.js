20818c1cbb34abd8b170a6e3ed392750f417c01f
Yulia Markina <jmarkina@gmail.com>
2016-12-17 21:11:37 -0800

    our approach page

--- assets/js/lib/main.js
+++ assets/js/lib/main.js
@@ -7,4 +7,11 @@ $(document).ready(function(){
 		$('#programsNav').css('display', 'none');
 		$('.nav-programs a').css('background-color', 'white')
 	});
+
+	// searching for orphans
+   $('p, h3, h4').each(function(){
+        var string = $(this).html();
+        string = string.replace(/ ([^ ]*)$/,'&nbsp;$1');
+        $(this).html(string);
+    });
 });
