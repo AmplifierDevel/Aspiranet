c311340c6bb47820999f7444ececad87e73766d9
Nicolas F <nicolas.funke@amplifierstrategies.com>
2016-12-20 15:09:15 -0800

    debug

--- build.sh
+++ build.sh
@@ -7,6 +7,7 @@ sed -i 's/ASPIRANET_CONTENTFUL_SPACE_ID/'"$ASPIRANET_CONTENTFUL_SPACE_ID"'/g' _c
 sed -i 's/ASPIRANET_CONTENTFUL_SPACE_ACCESS_TOKEN\ #Space/'"$ASPIRANET_CONTENTFUL_ACCESS_KEY"'/g' _config.yml 
 sed -i 's/ASPIRANET_CONTENTFUL_API_URL/'"$ASPIRANET_CONTENTFUL_API_URL"'/g' _config.yml 
 sed -i 's/ASPIRANET_CONTENTFUL_IS_PREVIEW/'"$ASPIRANET_CONTENTFUL_IS_PREVIEW"'/g' _config.yml 
+cat _config.yml 
 echo '### Fetching content & Building site ###'
 jekyll contentful --rebuild
 echo '### DONE ###'
