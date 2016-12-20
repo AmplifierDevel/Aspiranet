cf8bab55c4ef76c3452172aae99f132a81a43f5a
Nicolas F <nicolas.funke@amplifierstrategies.com>
2016-12-20 11:46:26 -0800

    adding new deploy

--- _grunt/shell.js
+++ _grunt/shell.js
@@ -6,10 +6,23 @@ module.exports = {
             command: 'jekyll contentful --rebuild'
     },
 	interpolateEnvVariables: {
-            command: `sed -i '' 's/ASPIRANET_CONTENTFUL_PREVIEW_KEY/'"$ASPIRANET_CONTENTFUL_PREVIEW_KEY"'/g' _config.yml && sed -i '' 's/ASPIRANET_CONTENTFUL_ACCESS_KEY/'"$ASPIRANET_CONTENTFUL_ACCESS_KEY"'/g' _config.yml && sed -i '' 's/ASPIRANET_CONTENTFUL_SPACE_ID/'"$ASPIRANET_CONTENTFUL_SPACE_ID"'/g' _config.yml`
+            command: `
+            sed -i '' 's/ASPIRANET_CONTENTFUL_PREVIEW_KEY/'"$ASPIRANET_CONTENTFUL_PREVIEW_KEY"'/g' _config.yml && 
+            sed -i '' 's/ASPIRANET_CONTENTFUL_ACCESS_KEY/'"$ASPIRANET_CONTENTFUL_ACCESS_KEY"'/g' _config.yml && 
+            sed -i '' 's/ASPIRANET_CONTENTFUL_SPACE_ID/'"$ASPIRANET_CONTENTFUL_SPACE_ID"'/g' _config.yml && 
+            sed -i '' 's/ASPIRANET_CONTENTFUL_SPACE_ACCESS_TOKEN/'"$ASPIRANET_CONTENTFUL_SPACE_ACCESS_TOKEN"'/g' _config.yml && 
+            sed -i '' 's/ASPIRANET_CONTENTFUL_API_URL/'"$ASPIRANET_CONTENTFUL_API_URL"'/g' _config.yml && 
+            sed -i '' 's/ASPIRANET_CONTENTFUL_IS_PREVIEW/'"$ASPIRANET_CONTENTFUL_IS_PREVIEW"'/g' _config.yml
+            `
     },
  	removeAPIKeys: {
-            command: `sed -i '' 's/'"$ASPIRANET_CONTENTFUL_PREVIEW_KEY"'/ASPIRANET_CONTENTFUL_PREVIEW_KEY/g' _config.yml && sed -i '' 's/'"$ASPIRANET_CONTENTFUL_ACCESS_KEY"'/ASPIRANET_CONTENTFUL_ACCESS_KEY/g' _config.yml && sed -i '' 's/'"$ASPIRANET_CONTENTFUL_SPACE_ID"'/ASPIRANET_CONTENTFUL_SPACE_ID/g' _config.yml`
+            command: `sed -i '' 's/'"$ASPIRANET_CONTENTFUL_PREVIEW_KEY"'/ASPIRANET_CONTENTFUL_PREVIEW_KEY/g' _config.yml && 
+            sed -i '' 's/'"$ASPIRANET_CONTENTFUL_ACCESS_KEY"'/ASPIRANET_CONTENTFUL_ACCESS_KEY/g' _config.yml && 
+            sed -i '' 's/'"$ASPIRANET_CONTENTFUL_SPACE_ID"'/ASPIRANET_CONTENTFUL_SPACE_ID/g' _config.yml && 
+            sed -i '' 's/'"$ASPIRANET_CONTENTFUL_SPACE_ACCESS_TOKEN"'/ASPIRANET_CONTENTFUL_SPACE_ACCESS_TOKEN/g' _config.yml && 
+            sed -i '' 's/'"$ASPIRANET_CONTENTFUL_API_URL"'/ASPIRANET_CONTENTFUL_API_URL/g' _config.yml && 
+            sed -i '' 's/'"$ASPIRANET_CONTENTFUL_IS_PREVIEW"'/ASPIRANET_CONTENTFUL_IS_PREVIEW/g' _config.yml
+            `
     },
     commentContentful: {
             command: `sed -i '' 's/"jekyll-contentful"/#"jekyll-contentful"/g' _config.yml && sed -i '' 's/gem "jekyll-contentful"/#gem "jekyll-contentful"/g' Gemfile && sed -i '' 's/gem "jekyll-contentful-data-import"/#gem "jekyll-contentful-data-import"/g' Gemfile`
