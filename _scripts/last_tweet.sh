a690db65e14f7dbd6ed572a0fb60c41956f82430
Nicolas F <nicolas.funke@amplifierstrategies.com>
2017-01-11 19:11:14 +0100

    debug

--- _scripts/last_tweet.sh
+++ _scripts/last_tweet.sh
@@ -18,6 +18,9 @@ APP_CREDS=`echo -n "$KEY:$SECRET" | base64`
 echo 'CREDS:'
 echo $APP_CREDS
 curl --silent --header "Authorization: Basic $APP_CREDS" --data "grant_type=client_credentials" https://api.twitter.com/oauth2/token > bearer_resp.json
+echo 'bearer_resp:'
+cat bearer_resp.json
+
 BEARER=`grep access_token\":\"[^\"]* bearer_resp.json --only-matching | sed s/access_token\":\"//g`
 
 echo 'BEARER'
