c7d387866787b40a4d3b7e3aadae9e674fb6462a
Nicolas F <nicolas.funke@amplifierstrategies.com>
2017-01-18 17:17:19 -0800

    adding filter

--- assets/js/lib/store-locator-custom.js
+++ assets/js/lib/store-locator-custom.js
@@ -7,6 +7,7 @@ MedicareDataSource.prototype.getStores = function(bounds, features, callback) {
   var that = this;
 
   $.getJSON( "../../assets/data/program_data.json", function( data ) {
+
     var stores = that.parse_(data);
     callback(stores);
   });
@@ -24,9 +25,21 @@ MedicareDataSource.prototype.getFeatures = function() {
 
 
 MedicareDataSource.prototype.parse_ = function(json) {
-  var stores = [];
+  var stores = [],
+      programs = [],
+      uniquePrograms = [],
+      filterByProgram = false,
+      filterByAge = false;
+
+    if(document.getElementById('programSelect').value != "") {
+       filterByProgram = true;
+    }
+    if(document.getElementById('ageSelect').value != "") {
+       filterByAge = true;
+    }
 
   json.items.forEach(function(item){
+
     var features = new storeLocator.FeatureSet;
     // features.add(this.FEATURES_.getById('Program-Residential'));
     var position = new google.maps.LatLng(item.fields.location.lat,
@@ -40,8 +53,34 @@ MedicareDataSource.prototype.parse_ = function(json) {
       item.fields.programAndAgeGroupAsText,
       web: item.fields.phoneText
     });
-    stores.push(store);
+
+
+    // Add all programs
+    if (item.fields.program) {
+    addAllItemsToArray(item.fields.program, programs);
+    }
+    // remove dupplicates
+    $.each(programs, function(i, el){
+      if($.inArray(el, uniquePrograms) === -1) uniquePrograms.push(el);
+    });
+
+    if (filterByProgram && !item.fields.program.includes(document.getElementById('programSelect').value)){
+      return;
+    }
+
+    if (item.fields.ageLevel) {
+      if (filterByAge && !item.fields.ageLevel.includes(document.getElementById('ageSelect').value)){
+        return;
+      }
+    }
+
+    stores.push(store);      
+
   });
+
+    if ($('#programSelect option').length == 1){
+      populateArray(uniquePrograms.sort(), $('#programSelect'));      
+    }
   
   return stores;
 };
@@ -58,4 +97,28 @@ storeLocator.FeatureSet.prototype.getById = function(featureId) {
     return this.array_[this.hash_[featureId]];
   }
   return null;
+};
+
+
+function populateArray(array, selectElement) {
+      var opt = document.createElement("option");
+       opt.value = "";
+       opt.innerHTML = "All";
+    selectElement.append(opt);
+
+    // Populate select
+    array.forEach(function(item){
+       var opt = document.createElement("option");
+       opt.value = item;
+       opt.innerHTML = item; // whatever property it has
+
+       // then append it to the select element
+       selectElement.append(opt);
+    });
+};
+
+function addAllItemsToArray(items, array){
+  items.forEach(function(item){
+      array.push(item);
+    });
 };
