3fdc6dc90d212541523fcb6c95a5999c317a34a7
Nicolas F <nicolas.funke@amplifierstrategies.com>
2017-01-25 16:06:45 -0800

    adding color for program submenu

--- assets/js/fixed-nav.js
+++ assets/js/fixed-nav.js
@@ -2,24 +2,13 @@ $(document).ready(function(){
 //programs menu to fixed on scroll
 
 	var fixTop = $('.programFixedNav').offset().top - 102;       
-	console.log('fixmeTop: '+ fixTop);
 	$(window).scroll(function() {                 
 
 	    var currentScroll = $(window).scrollTop(); 
-	    console.log('currentScroll: '+ currentScroll);
 	    if (currentScroll >= fixTop) { 
-	    	$('.programFixedNavPhantom').show();
-	        $('.programFixedNav').css({                      
-	            position: 'fixed',
-	            top: '100px',
-	            left: '0',
-	            right: '0'
-	        });
+	        $('.programFixedNav').addClass('fixed');
 	    } else {     
-	    	$('.programFixedNavPhantom').hide();    
-	        $('.programFixedNav').css({                      
-	            position: 'static'
-	        });
+	        $('.programFixedNav').removeClass('fixed');
 	    }
 	});
 });
