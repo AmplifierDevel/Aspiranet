4f5552b652efd86f248ed069071df7bd9888b36e
Nicolas F <nicolas.funke@amplifierstrategies.com>
2017-02-07 14:38:55 -0800

    fixing build staging

--- build.sh
+++ build.sh
@@ -12,11 +12,10 @@ echo '### Interpolating Variables ###'
 sed -i 's/ASPIRANET_CONTENTFUL_PREVIEW_KEY/'"$ASPIRANET_CONTENTFUL_PREVIEW_KEY"'/g' _config.yml 
 sed -i 's/ASPIRANET_CONTENTFUL_ACCESS_KEY/'"$ASPIRANET_CONTENTFUL_ACCESS_KEY"'/g' _config.yml 
 sed -i 's/ASPIRANET_CONTENTFUL_SPACE_ID/'"$ASPIRANET_CONTENTFUL_SPACE_ID"'/g' _config.yml 
-sed -i 's/ASPIRANET_CONTENTFUL_SPACE_ACCESS_TOKEN/'"$ASPIRANET_CONTENTFUL_ACCESS_KEY"'/g' _config.yml 
+sed -i 's/ASPIRANET_CONTENTFUL_SPACE_ACCESS_TOKEN/'"$ASPIRANET_CONTENTFUL_SPACE_ACCESS_TOKEN"'/g' _config.yml 
 sed -i 's/ASPIRANET_CONTENTFUL_API_URL/'"$ASPIRANET_CONTENTFUL_API_URL"'/g' _config.yml 
 sed -i 's/ASPIRANET_CONTENTFUL_IS_PREVIEW/'"$ASPIRANET_CONTENTFUL_IS_PREVIEW"'/g' _config.yml 
 echo '### Fetching content & Building site ###'
-cat _config.yml
 
 jekyll contentful --rebuild
 echo '### DONE ###'
