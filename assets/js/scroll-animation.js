9ae2f346d51bde492d726e4b80b410b0b0ebee13
Yulia Markina <jmarkina@gmail.com>
2017-02-12 19:08:25 +0300

    add slide up effect program pages

--- assets/js/scroll-animation.js
+++ assets/js/scroll-animation.js
@@ -1,20 +1,34 @@
 // Fadein effect on scroll 
 $(document).ready(function(){	
-	$(window).scroll( function(){
+
+    $(window).scroll( function(){
+        var bottom_of_window = $(window).scrollTop() + $(window).height();
+        
         $('.landInBlock').each(function(){
-            
             var bottom_of_object = $(this).position().top + $(this).outerHeight();
-            var bottom_of_window = $(window).scrollTop() + $(window).height();
             
             bottom_of_window = bottom_of_window + 0;  
           
             if( bottom_of_window > bottom_of_object ){
                 $(this).animate({
-                	'top': '0',
-                	'opacity':'1',
+                    'top': '0',
+                    'opacity':'1',
                     'easing': 'easeInExpo'
-                }, 600);       
+                },{ duration: 600})
+                .delay(600)
+                .queue(
+                    function() {
+                        $('.slideUpBlock').each(function(){
+                            $(this).animate({
+                                    'top': '0',
+                                    'opacity':'1',
+                                    'easing': 'easeOutQuart' 
+                            }, { duration: 600, queue: true});
+                        });
+                        return $(this).dequeue();
+                    }
+                );       
             }
         }); 
-    });
+    })
 });
