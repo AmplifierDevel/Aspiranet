08af09810b351545d971b8aa8bb9e521f1bd6745
Yulia Markina <jmarkina@gmail.com>
2017-02-13 17:56:46 +0300

    programs color block image move as mouse moves

--- assets/js/scroll-animation.js
+++ assets/js/scroll-animation.js
@@ -1,15 +1,27 @@
 // Fadein effect on scroll 
 $(document).ready(function(){	
 
+    //move color block on mouse move 
+
+    var bg = $('.moveBlock');
+    var windowWidth = $(window).innerWidth()/10;
+    var windowHeight = $(window).innerHeight()/10;
+   
+    $('.program-content').on('mousemove', function(e){
+        var mouseX = (e.clientX / windowWidth).toFixed(2);
+        var mouseY = (e.clientY / windowHeight).toFixed(2);
+        bg.css("transform","translate3d(" + mouseX +"px ,"+ mouseY + "px , 0px)");
+    }); 
+
+    //end of mouse move animation
+
     $(window).scroll( function(){
         var bottom_of_window = $(window).scrollTop() + $(window).height();
         
         $('.landInBlock').each(function(){
             var bottom_of_object = $(this).position().top + $(this).outerHeight();
             
-            bottom_of_window = bottom_of_window + 0;  
-          
-            if( bottom_of_window > bottom_of_object ){
+            if (bottom_of_window > bottom_of_object ) {
                 $(this).animate({
                     'top': '0',
                     'opacity':'1',
