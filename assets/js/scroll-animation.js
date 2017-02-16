c67d1316682e810f2851cc45ff049962bd7c6dba
Yulia Markina <jmarkina@gmail.com>
2017-02-16 20:09:57 +0300

    fade in images and hero for mobile

--- assets/js/scroll-animation.js
+++ assets/js/scroll-animation.js
@@ -1,23 +1,19 @@
 // Fadein effect on scroll 
 $(document).ready(function(){	
 
-    //move color block on mouse move 
-
-    var bg = $('.moveBlock');
-    var windowWidth = $(window).innerWidth()/10;
-    var windowHeight = $(window).innerHeight()/10;
-   
-    $('.program-content').on('mousemove', function(e){
-        var mouseX = (e.clientX / windowWidth).toFixed(2);
-        var mouseY = (e.clientY / windowHeight).toFixed(2);
-        bg.css("transform","translate3d(" + mouseX +"px ,"+ mouseY + "px , 0px)");
-    }); 
-
-    //end of mouse move animation
-
     $(window).scroll( function(){
         var bottom_of_window = $(window).scrollTop() + $(window).height();
         
+        $('.fadeInBlockOnScroll').each(function(){
+            var bottom_of_object = $(this).position().top + $(this).outerHeight();
+            if (bottom_of_window > bottom_of_object ) {
+                $(this).animate({
+                    'opacity':'1',
+                    'easing': 'easeOutCirc'
+                }, 800);
+            }
+        });
+
         $('.landInBlock').each(function(){
             var bottom_of_object = $(this).position().top + $(this).outerHeight();
             
